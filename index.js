fe1c660c7318cb02eb5a0fe399adef9f6c7687f2
Matt Draghi <mtd5264@gmail.com>
2022-01-24 11:51:15 -0500

    Added all beer data to card

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const baseURL = 'https://api.sampleapis.com/beers/ale/';
 const main = document.querySelector('div.main')
 
 const fetchAles = () =>{
-    for(let id = 1; id < 5; id ++){
+    for(let id = 3; id < 7; id ++){
         fetch(`${baseURL}${id}`)
         .then(resp => resp.json())
         .then(beerData => {
@@ -12,9 +12,30 @@ const fetchAles = () =>{
 }
 
 const createCard = (beerData) => {
+    //create card for beer
     const newCard = document.createElement('div')
     newCard.setAttribute('class', 'card')
-    newCard.innerText = beerData
+    console.log(beerData)
+    //add beer image
+    const img = document.createElement('img')
+    img.src = beerData.image
+    img.alt = beerData.name
+    newCard.appendChild(img)
+    //add the name
+    const name = document.createElement('p')
+    name.innerText = beerData.name
+    newCard.appendChild(name)
+    
+    //add the price
+    const price = document.createElement('span')
+    price.innerText = ` ${beerData.price}`
+    name.append(price)
+
+    //add the rating
+    const rating = document.createElement('p')
+    rating.innerText = `${beerData.rating.average.toFixed(2)} / 5`
+    newCard.appendChild(rating)
+    //add card to main div
     main.appendChild(newCard)
 }
 
