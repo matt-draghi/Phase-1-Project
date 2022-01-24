7b8dc6899dd041d3dc731e299164087e594e8797
Matt Draghi <mtd5264@gmail.com>
2022-01-24 17:55:09 -0500

    Fixed styling - attempted to add icons

--- index.js
+++ index.js
@@ -16,44 +16,69 @@ const fetchAles = () =>{
     })
 }
 
-const createCard = (beerData) => {
+const createCard = (beer) => {
     //create card for beer
     const newCard = document.createElement('div')
     newCard.setAttribute('class', 'card')
-    console.log(beerData)
+    console.log(beer)
 
+    renderImage(beer, newCard)
+    renderInfo(beer, newCard)
+    renderRating(beer, newCard)
+    renderLike(newCard)   
+
+    //add card to main div
+    main.appendChild(newCard)
+    console.log(typeof(newCard))
+    console.log(newCard)
+}
+
+const renderImage = (beer, newCard) =>{
     //add beer image to separate div within the card
     const imgDiv = document.createElement('div')
     imgDiv.setAttribute('class', 'card_image')
+
     const img = document.createElement('img')
-    img.src = beerData.image
-    img.alt = beerData.name
+    img.src = beer.image
+    img.alt = beer.name
+
     imgDiv.appendChild(img)
     newCard.appendChild(imgDiv)
-   
+}
+
+const renderInfo = (beer, newCard) => {
     //create div to hold other info in card
     const infoDiv = document.createElement('div')
-    infoDiv.setAttribute=('class', 'card_info')
+    infoDiv.setAttribute('class', 'card_info')
+
     //add the name
     const name = document.createElement('p')
-    name.innerText = beerData.name
+    name.innerText = beer.name
     infoDiv.appendChild(name)
     
     //add the price
-    const price = document.createElement('span')
-    price.innerText = ` ${beerData.price}`
-    name.append(price)
-
-    //add the rating
-    const rating = document.createElement('p')
-    rating.innerText = `${beerData.rating.average.toFixed(2)} / 5`
-    infoDiv.appendChild(rating)
-
+    const price = document.createElement('p')
+    price.setAttribute('id', 'price')
+    price.innerText = ` ${beer.price}`
+    infoDiv.append(price)
+    //append info to the card
     newCard.appendChild(infoDiv)
-    //add card to main div
-    main.appendChild(newCard)
 }
 
+const renderRating = (beer, newCard) => {
+    const ratingDiv = document.createElement('div')
+    const rating = document.createElement('p')
+    rating.innerText = `${beer.rating.average.toFixed(2)} / 5`
+    ratingDiv.appendChild(rating)
+
+    newCard.appendChild(ratingDiv)
+}
+
+const renderLike = (newCard) => {
+    const likeBttn = document.createElement('i')
+    likeBttn.setAttribute('class', 'far fa-heart')
+    newCard.appendChild(likeBttn)
+}
 
 //Invoke Functions
 const init = () =>{
