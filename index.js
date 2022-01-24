1a5abfad7d113340398af57ac6f901be2a3a8638
Matt Draghi <mtd5264@gmail.com>
2022-01-24 11:22:13 -0500

    - Created header with style - Sidebar created with style - Main area created with style - Fetched initial data for first 4 beers - Created function to create cards for each fetched beer

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const baseURL = 'https://api.sampleapis.com/beers/ale/';
+const main = document.querySelector('div.main')
 
 const fetchAles = () =>{
     for(let id = 1; id < 5; id ++){
         fetch(`${baseURL}${id}`)
         .then(resp => resp.json())
-        .then(data => {
-            console.log(data)
+        .then(beerData => {
+            createCard(beerData)
         })
     }
 }
 
-fetchAles()
+const createCard = (beerData) => {
+    const newCard = document.createElement('div')
+    newCard.setAttribute('class', 'card')
+    newCard.innerText = beerData
+    main.appendChild(newCard)
+}
+
+
+//Invoke Functions
+const init = () =>{
+    fetchAles()
+}
+
+init()
