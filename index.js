cfdce878aecd6670cd76c65157b9c00cb82be905
Matt Draghi <mtd5264@gmail.com>
2022-01-26 12:06:16 -0500

    Refactored large else statement to be more manageable

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const createCard = (beer) => {
     //create card for beer
     const newCard = document.createElement('div')
     newCard.setAttribute('class', 'card')
-    //newCard.setAttribute()
-    console.log(beer)
-
     renderImage(beer, newCard)
     renderInfo(beer, newCard)
     renderRating(beer, newCard)
@@ -76,12 +73,9 @@ const renderButtons = (beer, newCard) => {
     const likeBttn = document.createElement('button') 
     div.appendChild(likeBttn)
     likeBttn.setAttribute('id', 'like')
-    likeBttn.innerHTML = `
-        <i class="far fa-heart"> Like </i>
-    `
+    likeBttn.innerHTML = `<i class="far fa-heart"> Like </i>`
     likeBttn.addEventListener('click', () =>{
         let likeState = likeBttn.querySelector(`i`).attributes.class.value
-        //console.log(likeState)
         if(likeState === "far fa-heart"){
             likeBttn.querySelector(`i`).setAttribute('class', 'fas fa-heart')
         }
@@ -93,109 +87,132 @@ const renderButtons = (beer, newCard) => {
     //create review button
     const reviewBttn = document.createElement(`button`)
     div.appendChild(reviewBttn)
-    reviewBttn.innerHTML = `
-        <i class="far fa-star">Review</i>
-    `
+    reviewBttn.innerHTML = `<i class="far fa-star">Review</i>`
     reviewBttn.setAttribute(`id`, `review`)
     
     //add event listener
     reviewBttn.addEventListener('click', () => {
-        if(document.querySelector(`div[id="${beer.name}"]`)){
+        if(document.querySelector(`div[name="${beer.name}"]`)){
             alert('You have already reviewed this beer.')
         }
         else{
-            //create modal container
-            const reviewForm = document.createElement('div')
-            reviewForm.setAttribute(`id`, `${beer.name}`)
-            reviewForm.setAttribute(`class`, `modal`)
-            //create modal content
-            const reviewFormContent = document.createElement(`div`)
-            reviewFormContent.setAttribute(`class`, `modal-content`)
-            reviewForm.appendChild(reviewFormContent)
-            
-             //create button to close modal
-            const closeReviewForm = document.createElement(`div`)
-            closeReviewForm.setAttribute(`class`, `close`)
-            closeReviewForm.innerHTML = `&times;`
-            reviewFormContent.appendChild(closeReviewForm)
-            closeReviewForm.addEventListener('click', () => {
-                reviewForm.style.display = "none"
-            })
-
-            //add h1 form header to modal
-            const formHeader = document.createElement('h1')
-            formHeader.innerText = `Please leave your review of ${beer.name}:`
-            reviewFormContent.appendChild(formHeader)
-
-            //create radioDiv to group the radios
-            const radioDiv = document.createElement('div')
-            radioDiv.setAttribute(`class`, 'radioDiv')
-
-             // radio buttons
-            for(let reviewOption = 0; reviewOption < 6; reviewOption++){
-                const option = document.createElement(`input`)
-                const label = document.createElement(`label`)
-                option.setAttribute(`type`, `radio`)
-                option.setAttribute(`id`, `${reviewOption}`)
-                option.setAttribute(`name`, `rating`)
-                option.setAttribute(`value`, `${reviewOption}`)
-                if(reviewOption === 0){
-                    option.required = true
-                }
-                label.setAttribute(`For`, `${reviewOption}`)
-                label.innerText = `${reviewOption}`
-                
-                //radios appended to radioDiv
-                radioDiv.appendChild(option)
-                radioDiv.appendChild(label)
-            }
-
-            // create the form; radioDiv appends to form; from appends to reviewFormContent (modal)
-            const form = document.createElement('form')
-            form.appendChild(radioDiv)
-            reviewFormContent.appendChild(form)
-            
-            // create form input
-            const formInput = document.createElement('input')
-            formInput.setAttribute(`class`, 'text-input')
-            formInput.setAttribute(`placeholder`, 'Additional comments...')
-            form.appendChild(formInput)
-
-            // create submit listener
-            form.addEventListener('submit', (e) => {
-                e.preventDefault()
-                const userRating = document.querySelector(".radioDiv input[type='radio'][name='rating']:checked").value;
-                const totalRating = (beer.rating.average * beer.rating.reviews) + userRating
-                beer.rating.reviews += 1
-                newAverage = totalRating / beer.rating.reviews
-
-                const rating = document.getElementById(`${beer.id} Rating`)
-                rating.innerText = `${beer.rating.average.toFixed(2)} / 5    (${beer.rating.reviews} reviews)`
-
-                /*TODO: make it so another modal opens on submit 
-                with confirmation of submit, followed by close - setTimeout will work*/
-                reviewForm.style.display = "none"
-            })
-
-            //create submit button
-            const submitBttn = document.createElement('button')
-            submitBttn.innerText = 'Submit'         
-            form.appendChild(submitBttn)
-
-            //close modal on window click
-            window.addEventListener('click', (e) => {
-                if(e.target === reviewForm){
-                    reviewForm.style.display = "none"
-                }
-            })
-
-            main.appendChild(reviewForm)
-            reviewForm.style.display = "block"
+            createModal(beer)
         }
     })
     
     //append new div to card
     newCard.appendChild(div)
+}
+
+const createModal = (beer) => {
+    //create modal container
+    const reviewForm = document.createElement('div')
+    reviewForm.setAttribute(`id`, `${beer.id}`)
+    reviewForm.setAttribute(`class`, `modal`)
+
+    addModalContent(reviewForm, beer)
+    main.appendChild(reviewForm)
+
+    //close modal on window click
+    window.addEventListener('click', (e) => {
+        if(e.target === reviewForm){
+            reviewForm.style.display = "none"
+        }
+    })
+
+    reviewForm.style.display = "block"
+}
+
+const addModalContent = (modal, beer) => {
+    //create modal content
+    const reviewFormContent = document.createElement(`div`)
+    reviewFormContent.setAttribute(`class`, `modal-content`)
+    modal.appendChild(reviewFormContent)
+
+    //create button to close modal
+    const closeReviewForm = document.createElement(`div`)
+    closeReviewForm.setAttribute(`class`, `close`)
+    closeReviewForm.innerHTML = `&times;`
+    reviewFormContent.appendChild(closeReviewForm)
+    closeReviewForm.addEventListener('click', () => {
+        modal.style.display = "none"
+    })
+
+    //add h1 form header to modal
+    const formHeader = document.createElement('h1')
+    formHeader.innerText = `Please leave your review of ${beer.name}:`
+    reviewFormContent.appendChild(formHeader)
+
+    addFormToModal(reviewFormContent)
+}
+
+const addFormToModal = (modalContent) => {
+    //create radioDiv to group the radios
+    const radioDiv = document.createElement('div')
+    radioDiv.setAttribute(`class`, 'radioDiv')
+    
+    // radio buttons
+    for(let reviewOption = 0; reviewOption < 6; reviewOption++){
+        const option = document.createElement(`input`)
+        const label = document.createElement(`label`)
+        option.setAttribute(`type`, `radio`)
+        option.setAttribute(`id`, `${reviewOption}`)
+        option.setAttribute(`name`, `rating`)
+        option.setAttribute(`value`, `${reviewOption}`)
+        if(reviewOption === 0){
+            option.required = true
+        }
+        label.setAttribute(`For`, `${reviewOption}`)
+        label.innerText = `${reviewOption}`
+        
+        //radios appended to radioDiv
+        radioDiv.appendChild(option)
+        radioDiv.appendChild(label)
+    }
+
+    // create the form; radioDiv appends to form; from appends to reviewFormContent (modal)
+    const form = document.createElement('form')
+    form.appendChild(radioDiv)
+    modalContent.appendChild(form)
+
+    // create form input
+    const formInput = document.createElement('input')
+    formInput.setAttribute(`class`, 'text-input')
+    formInput.setAttribute(`placeholder`, 'Additional comments...')
+    form.appendChild(formInput)
+
+    //create submit listener
+    form.addEventListener('submit', handleSubmit)
+    
+    //create submit button
+    const submitBttn = document.createElement('button')
+    submitBttn.innerText = 'Submit'         
+    form.appendChild(submitBttn)
+}
+
+//TODO: refactor
+const handleSubmit = (e) => {
+    e.preventDefault()
+    //grab id from target parent element and use this to fetch data for a specific id
+    const beerId = e.target.offsetParent.id
+    fetch(baseURL+beerId)
+    .then(resp => resp.json())
+    .then(beer => {
+        const userRating = document.querySelector(".radioDiv input[type='radio'][name='rating']:checked").value;
+        const totalRating = (beer.rating.average * beer.rating.reviews) + userRating
+        beer.rating.reviews += 1
+        newAverage = totalRating / beer.rating.reviews
+        const rating = document.getElementById(`${beer.id} Rating`)
+        rating.innerText = `${beer.rating.average.toFixed(2)} / 5    (${beer.rating.reviews} reviews)`
+
+        /*TODO: make it so another modal opens on submit 
+        with confirmation of submit, followed by close - setTimeout will work*/
+
+        const reviewForm = e.path[2]
+        //set form name upon successful submission to serve as validation for if statement in renderButtons()
+        reviewForm.setAttribute(`name`, `${beer.name}`)
+        reviewForm.style.display = "none"
+    })
 }
 
 //Invoke Functions
